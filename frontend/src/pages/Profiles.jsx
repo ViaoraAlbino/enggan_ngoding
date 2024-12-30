9f1ba91a80beae93dc381edb3b18fd90af9985a9
ViaoraAlbino <mqolbishufi@gmail.com>
2024-12-30 20:26:32 +0700

    hari ke 9

--- frontend/src/pages/Profiles.jsx
+++ frontend/src/pages/Profiles.jsx
@@ -1,76 +1,63 @@
 import React, { useState } from "react";
 import Foto from "../assets/gambar/me.jpg";
+import { width } from "@fortawesome/free-solid-svg-icons/fa0";
 
 const Profiles = () => {
-  // State untuk menentukan konten yang aktif
   const [activeSection, setActiveSection] = useState("profile");
 
-  // Simulasi konten untuk setiap section
   const sections = {
     profile: (
-      <div>
-        <h1 className="text-left font-poppins-profil-isi mt-6">Profil Saya</h1>
-        <table className="border-none">
-          <tr>
-            <td>
-              <p className="font-poppins-menu-navigasi-header">Nama</p>
-            </td>
-            <td>
-            <div class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
-      <div class="shrink-0 select-none text-base text-gray-500 sm:text-sm/6">$</div>
-      <input type="text" name="price" id="price" class="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" placeholder="0.00" />
-      <div class="grid shrink-0 grid-cols-1 focus-within:relative">
-        <select id="currency" name="currency" aria-label="Currency" class="col-start-1 row-start-1 w-full appearance-none rounded-md py-1.5 pl-3 pr-7 text-base text-gray-500 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
-          <option>USD</option>
-          <option>CAD</option>
-          <option>EUR</option>
-        </select>
-        <svg class="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon">
-          <path fill-rule="evenodd" d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
-        </svg>
-      </div>
-    </div>
-            </td>
-          </tr>
-        </table>
-        {/* <form className="space-y-4">
-          <div>
-            <label className="block font-medium text-gray-700">Nama</label>
+      <div className="px-6 py-6">
+        <h1 className="text-left font-poppins-profil-isi mt-6 text-2xl font-semibold">Profil Saya</h1>
+        <div className="mt-8 mb-10 space-y-6">
+          <div className="flex items-center space-x-4">
+            <p className="font-poppins-menu-navigasi-header text-left w-1/4">Nama</p>
             <input
               type="text"
-              className="w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
-              placeholder="Adam"
-              value="Adam"
+              className="font-poppins-profil-isi-form mt-1 block w-3/4 px-3 py-4 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+          focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+          disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+          invalid:border-pink-500 invalid:text-pink-600
+          focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
             />
           </div>
-          <div>
-            <label className="block font-medium text-gray-700">Username</label>
+
+          <div className="flex items-center space-x-4">
+            <p className="font-poppins-menu-navigasi-header text-left w-1/4">Username</p>
             <input
               type="text"
-              className="w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
-              placeholder="Adam"
-              value="Adam"
+              className="font-poppins-profil-isi-form mt-1 block w-3/4 px-3 py-4 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+          focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+          disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+          invalid:border-pink-500 invalid:text-pink-600
+          focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
             />
           </div>
-          <div>
-            <label className="block font-medium text-gray-700">Email</label>
-            <input
-              type="email"
-              className="w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
-              placeholder="adam@gmail.com"
-              value="adam@gmail.com"
-            />
-          </div>
-          <div>
-            <label className="block font-medium text-gray-700">No Telepon</label>
+
+          <div className="flex items-center space-x-4">
+            <p className="font-poppins-menu-navigasi-header text-left w-1/4">Email</p>
             <input
               type="text"
-              className="w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
-              placeholder="0888 - 8888 - 8888"
-              value="0888 - 8888 - 8888"
+              className="font-poppins-profil-isi-form mt-1 block w-3/4 px-3 py-4 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+          focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+          disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+          invalid:border-pink-500 invalid:text-pink-600
+          focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
             />
           </div>
-        </form> */}
+
+          <div className="flex items-center space-x-4">
+            <p className="font-poppins-menu-navigasi-header text-left w-1/4">No Telpon</p>
+            <input
+              type="number"
+              className="font-poppins-profil-isi-form mt-1 block w-3/4 px-3 py-4 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+          focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+          disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+          invalid:border-pink-500 invalid:text-pink-600
+          focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+            />
+          </div>
+        </div>
       </div>
     ),
     address: (
@@ -80,15 +67,107 @@ const Profiles = () => {
       </div>
     ),
     "bank-info": (
-      <div>
-        <h1 className="text-2xl font-semibold mb-4">Info Rekening</h1>
-        <p>Belum ada data rekening yang tersedia.</p>
+      <div className="px-6 py-6">
+        <h1 className="text-left font-poppins-profil-isi mt-6 text-2xl font-semibold">Rekening Bank</h1>
+        <div className="mt-8 mb-10 space-y-6">
+          <div className="flex items-center space-x-4">
+            <p className="font-poppins-menu-navigasi-header text-left w-2/4">Nama Bank</p>
+            <input
+              type="text"
+              className="font-poppins-profil-isi-form mt-1 block w-3/4 px-3 py-4 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+          disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+              invalid:border-pink-500 invalid:text-pink-600
+              focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+            />
+          </div>
+          <div className="flex items-center space-x-4">
+            <p className="font-poppins-menu-navigasi-header text-left w-2/4">Nama Cabang</p>
+            <input
+              type="text"
+              className="font-poppins-profil-isi-form mt-1 block w-3/4 px-3 py-4 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+            disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+              invalid:border-pink-500 invalid:text-pink-600
+              focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+            />
+          </div>
+
+          <div className="flex items-center space-x-4">
+            <p className="font-poppins-menu-navigasi-header text-left w-2/4">Nama Lengkap</p>
+            <input
+              type="text"
+              className="font-poppins-profil-isi-form mt-1 block w-3/4 px-3 py-4 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+              disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+              invalid:border-pink-500 invalid:text-pink-600
+              focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+            />
+          </div>
+          <div className="flex items-center space-x-4">
+            <p className="font-poppins-menu-navigasi-header text-left w-2/4">Nomor Rekening</p>
+            <input
+              type="number"
+              className="font-poppins-profil-isi-form mt-1 block w-3/4 px-3 py-4 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+              disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+              invalid:border-pink-500 invalid:text-pink-600
+              focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+            />
+          </div>
+          <div className="text-left">
+            <button className="font-monster-button-password mt-7 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 px-6 py-4 rounded-2xl">
+              Simpan Data
+            </button>
+          </div>
+        </div>
       </div>
     ),
     "change-password": (
-      <div>
-        <h1 className="text-2xl font-semibold mb-4">Ganti Password</h1>
-        <p>Form untuk mengganti password akan ditambahkan nanti.</p>
+      <div className="px-6 py-6">
+        <h1 className="text-left font-poppins-profil-isi mt-6 text-2xl font-semibold">Ganti Password</h1>
+        <div className="mt-8 mb-10 space-y-6">
+          <div className="flex items-center space-x-4">
+            <p className="font-poppins-menu-navigasi-header text-left w-2/4">Password Lama</p>
+            <input
+              type="text"
+              className="font-poppins-profil-isi-form mt-1 block w-3/4 px-3 py-4 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+              disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+              invalid:border-pink-500 invalid:text-pink-600
+              focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+            />
+          </div>
+
+          <div className="flex items-center space-x-4">
+            <p className="font-poppins-menu-navigasi-header text-left w-2/4">Password Baru</p>
+            <input
+              type="text"
+              className="font-poppins-profil-isi-form mt-1 block w-3/4 px-3 py-4 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+              disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+              invalid:border-pink-500 invalid:text-pink-600
+              focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+            />
+          </div>
+
+          <div className="flex items-center space-x-4">
+            <p className="font-poppins-menu-navigasi-header text-left w-2/4">Konfirmasi Password</p>
+            <input
+              type="text"
+              className="font-poppins-profil-isi-form mt-1 block w-3/4 px-3 py-4 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+              disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+              invalid:border-pink-500 invalid:text-pink-600
+              focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+            />
+          </div>
+          <div className="text-left">
+            <button className="font-monster-button-password mt-7 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 px-6 py-4 rounded-2xl">
+              Simpan Password Baru
+            </button>
+          </div>
+        </div>
       </div>
     ),
     "purchase-history": (
@@ -111,7 +190,7 @@ const Profiles = () => {
                 <li>
                   <hr className="w-full border-black" />
                   <h2 className="font-poppins-menu-navigasi-header text-left text-lg font-bold mt-2 w-full border-t border-b px-4 py-6">
-                    Adam
+                    Hi, Adam
                   </h2>
                   <hr className="w-full border-black mb-4" />
                 </li>
@@ -120,11 +199,10 @@ const Profiles = () => {
                 </li>
                 <li>
                   <button
-                    className={`block font-poppins-menu-navigasi-body w-full text-left py-2 px-4 ${
-                      activeSection === "profile"
-                        ? "bg-blue-200 text-blue-800"
-                        : "text-gray-700 hover:bg-blue-200 hover:text-blue-800"
-                    }`}
+                    className={`block font-poppins-menu-navigasi-body w-full text-left py-2 px-4 ${activeSection === "profile"
+                      ? "bg-blue-200 text-blue-800"
+                      : "text-gray-700 hover:bg-blue-200 hover:text-blue-800"
+                      }`}
                     onClick={() => setActiveSection("profile")}
                   >
                     Profil Saya
@@ -132,11 +210,10 @@ const Profiles = () => {
                 </li>
                 <li>
                   <button
-                    className={`block font-poppins-menu-navigasi-body w-full text-left py-2 px-4 ${
-                      activeSection === "address"
-                        ? "bg-blue-200 text-blue-800"
-                        : "text-gray-700 hover:bg-blue-200 hover:text-blue-800"
-                    }`}
+                    className={`block font-poppins-menu-navigasi-body w-full text-left py-2 px-4 ${activeSection === "address"
+                      ? "bg-blue-200 text-blue-800"
+                      : "text-gray-700 hover:bg-blue-200 hover:text-blue-800"
+                      }`}
                     onClick={() => setActiveSection("address")}
                   >
                     Alamat
@@ -144,11 +221,10 @@ const Profiles = () => {
                 </li>
                 <li>
                   <button
-                    className={`block font-poppins-menu-navigasi-body w-full text-left py-2 px-4 ${
-                      activeSection === "bank-info"
-                        ? "bg-blue-200 text-blue-800"
-                        : "text-gray-700 hover:bg-blue-200 hover:text-blue-800"
-                    }`}
+                    className={`block font-poppins-menu-navigasi-body w-full text-left py-2 px-4 ${activeSection === "bank-info"
+                      ? "bg-blue-200 text-blue-800"
+                      : "text-gray-700 hover:bg-blue-200 hover:text-blue-800"
+                      }`}
                     onClick={() => setActiveSection("bank-info")}
                   >
                     Info Rekening
@@ -156,26 +232,24 @@ const Profiles = () => {
                 </li>
                 <li>
                   <button
-                    className={`block font-poppins-menu-navigasi-body w-full text-left py-2 px-4 ${
-                      activeSection === "change-password"
-                        ? "bg-blue-200 text-blue-800"
-                        : "text-gray-700 hover:bg-blue-200 hover:text-blue-800"
-                    }`}
+                    className={`block font-poppins-menu-navigasi-body w-full text-left py-2 px-4 ${activeSection === "change-password"
+                      ? "bg-blue-200 text-blue-800"
+                      : "text-gray-700 hover:bg-blue-200 hover:text-blue-800"
+                      }`}
                     onClick={() => setActiveSection("change-password")}
                   >
                     Ganti Password
                   </button>
                 </li>
                 <li>
-                  <p className="font-poppins-menu-navigasi-header px-4 py-2 text-left">Akun Saya</p>
+                  <p className="font-poppins-menu-navigasi-header px-4 py-2 text-left">Pembelian</p>
                 </li>
                 <li>
                   <button
-                    className={`block font-poppins-menu-navigasi-body w-full text-left py-2 px-4 ${
-                      activeSection === "purchase-history"
-                        ? "bg-blue-200 text-blue-800"
-                        : "text-gray-700 hover:bg-blue-200 hover:text-blue-800"
-                    }`}
+                    className={`block font-poppins-menu-navigasi-body w-full text-left py-2 px-4 ${activeSection === "purchase-history"
+                      ? "bg-blue-200 text-blue-800"
+                      : "text-gray-700 hover:bg-blue-200 hover:text-blue-800"
+                      }`}
                     onClick={() => setActiveSection("purchase-history")}
                   >
                     Riwayat Pembelian
@@ -188,7 +262,7 @@ const Profiles = () => {
 
         {/* Main Content */}
         <main className="flex-1 pt-16 px-8 mr-8">
-          <div className="bg-white px-6 py-6 rounded-lg shadow-lg w-full">
+          <div className="bg-white px-5 py-5 rounded-xl shadow-lg w-full">
             {sections[activeSection]}
           </div>
         </main>
